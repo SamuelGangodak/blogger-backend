b465590b80911a0d74adcb5e01e827e4b916c457
SamuelGangodak <samuelgangodak79@gmail.com>
2024-07-22 15:28:07 +0530

    changed files

--- index.js
+++ index.js
@@ -23,11 +23,12 @@ app.use(cors())
 app.use('/uploads', express.static('uploads'))
 
 app.get("/", (req, res)=>{
+    res.set('Access-Control-Allow-Origin', '*');
     res.json({"message":"Hello World!"});
 })
 
 app.get("/blog/:cat", async (req, res)=>{
-    
+    res.set('Access-Control-Allow-Origin', '*');
     const result=await db.query(
         req.params.cat!='all' ? `SELECT * FROM blogs where category = '${req.params.cat}' ORDER by id DESC` : 'SELECT * FROM blogs ORDER by id DESC'
     );
@@ -35,16 +36,19 @@ app.get("/blog/:cat", async (req, res)=>{
 })
 
 app.get("/blog/id/:id", async (req, res)=>{
+    res.set('Access-Control-Allow-Origin', '*');
     const result=await db.query(`SELECT * FROM blogs WHERE id = ${req.params.id} ORDER by id DESC`);
     res.json({"data": result.rows});
 })
 
 app.post("/blog", async (req, res)=>{
+    res.set('Access-Control-Allow-Origin', '*');
     const result=await db.query('INSERT INTO blogs (title, image, post, category) VALUES($1, $2, $3, $4)', [req.body.title, req.body.image, req.body.post, req.body.category]);
     res.json({"message": "Added a new blog", "desc": result.rowCount});
 })
 
 app.post('/blogimage', upload.single('file'), function (req, res, next) {
+    res.set('Access-Control-Allow-Origin', '*');
     res.json(req.file)
 })
 
