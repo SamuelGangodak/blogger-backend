c85c5141ad4f2f2037eeb77cf9611b92bae70c15
SamuelGangodak <samuelgangodak79@gmail.com>
2024-07-22 14:33:37 +0530

    modified connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,27 @@
-import pg from "pg";
+//import pg from "pg";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db=new pg.Client({
+// const db=new pg.Client({
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   password: process.env.PG_PASSWORD,
+//   port: process.env.PG_PORT,
+// });
+
+// async function check() {
+//     await db.connect();
+// }
+
+// check();
+// export default db;
+
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
@@ -11,9 +29,8 @@ const db=new pg.Client({
   port: process.env.PG_PORT,
 });
 
-async function check() {
-    await db.connect();
-}
-
-check();
-export default db;
+// const db=await pool.connect();
+// export default db;
+export default {
+  query: (text, params) => pool.query(text, params),
+};
